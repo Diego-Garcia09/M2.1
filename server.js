04ee720acd6c57b5d61f1a03f2baeccb58da0b22
Diego-Garcia09 <112862634+Diego-Garcia09@users.noreply.github.com>
2023-09-19 01:22:29 -0700

    M2.1 Terminado

--- server.js
+++ server.js
@@ -1,44 +1,13 @@
-//const http = require('http');
-
-// Define una función para manejar las solicitudes entrantes
-//const manejarSolicitud = (req, res) => {
-//  res.writeHead(200, { 'Content-Type': 'text/plain' });
-//  res.end('¡Hola, mundo!\n');
-//};
-
-// Crea un servidor HTTP y pasa la función de manejo de solicitudes
-//const servidor = http.createServer(manejarSolicitud);
-
-// Especifica el puerto en el que deseas ejecutar el servidor
-//const puerto = 8080;
-
-// Escucha en el puerto especificado
-//servidor.listen(puerto, () => {
-//  console.log(`Servidor HTTP escuchando en el puerto ${puerto}`);
-//});
-
 const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
 
-// Datos de ejemplo para simular una "base de datos"
-const datos = {};
-
 const servidor = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true);
-    //console.log(query);//Aqui consigues lo del enlace
-
-
   if (req.method === 'GET') {
     // Manejo de solicitudes GET
     if (pathname === '/datos') {
-      console.log(query);
-      console.log(pathname);
-      console.log(pathname, query);
-      console.log(req.url);
-      console.log(req.headers);
-      // Devuelve los datos almacenados como JSON
-      res.writeHead(200, { 'Content-Type': 'text/html' });//Cambia text/plain a text/html
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(`
       <!DOCTYPE html>
       <html>
@@ -51,45 +20,62 @@ const servidor = http.createServer((req, res) => {
         <body>
           <h1>Request headers:</h1>
           <p>${JSON.stringify(req.headers, null, 2).split(",").join("<br>")}</p>
-          <h2>Metodo:${req.method}</h2>
-          <h3>URL:${req.url}</h3>
+          <h2>Método: ${req.method}</h2>
+          <h3>URL: ${req.url}</h3>
         </body>
-      </html>`);//Aqui va el html
+      </html>`);
+    } else if (pathname === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/html'});
+      res.end(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE-Edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Datos</title>
+        </head>
+        <body>
+          <h1>Enviando datos al servidor HTTP</h1>
+          <p>Datos para el server: </p>
+          <form method="post" action="/">
+              <label for="dato">Dato:</label>
+              <input type="text" id="dato" name="dato">
+              <button type="submit">Enviar</button>
+            </form>
+        </body>
+      </html>
+      `);
     } else {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('Ruta no encontrada');
     }
   } else if (req.method === 'POST') {
     // Manejo de solicitudes POST
-    if (pathname === '/datos') {
+    if (pathname === '/') {
       let cuerpo = '';
       req.on('data', (chunk) => {
         cuerpo += chunk;
       });
       req.on('end', () => {
-        const nuevoDato = querystring.parse(cuerpo);
-        // Almacena el nuevo dato
-        datos[Object.keys(nuevoDato)[0]] = nuevoDato[Object.keys(nuevoDato)[0]];
-        res.writeHead(201, { 'Content-Type': 'text/plain' });
-        res.end('Dato almacenado exitosamente');//Aqui va el html de respuesta
-      });
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Ruta no encontrada');
-    }
-  } else if (req.method === 'PUT') {
-    // Manejo de solicitudes PUT
-    if (pathname === '/datos') {
-      let cuerpo = '';
-      req.on('data', (chunk) => {
-        cuerpo += chunk;
-      });
-      req.on('end', () => {
-        const nuevoDato = querystring.parse(cuerpo);
-        // Actualiza el dato existente
-        datos[Object.keys(nuevoDato)[0]] = nuevoDato[Object.keys(nuevoDato)[0]];
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Dato actualizado exitosamente');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE-Edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Datos</title>
+          </head>
+          <body>
+            <h1>Request headers:</h1>
+            <p>${JSON.stringify(req.headers, null, 2).split(",").join("<br>")}</p>
+            <h2>Método: ${req.method}</h2>
+            <h3>URL: ${req.url}</h3>
+            <h4>Datos enviados: ${cuerpo}</h4>
+          </body>
+        </html>`);
       });
     } else {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
